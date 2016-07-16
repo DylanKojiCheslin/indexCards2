4d9d801d978faa137b8a6ce1962fd8b5264229e9
dylan <dylan.kojicheslin@gmail.com>
2016-07-16 07:09:43 -0600

    imports/file structure fixed #1

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,2 +1,12 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// import '/imports/startup/client/ui/layouts.appBody.js';
+import '../../ui/layouts/appBody.js';
+
+FlowRouter.route('/', {
+  name: 'App.home',
+  action() {
+    BlazeLayout.render('appBody');
+  },
+});
