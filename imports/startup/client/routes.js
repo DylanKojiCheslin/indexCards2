31a2e2aac8d8eca98397ffbfc6de237946eb15e9
dylan <dylan.kojicheslin@gmail.com>
2016-07-17 00:35:01 -0600

    cruft

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,7 +1,6 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-// import '/imports/startup/client/ui/layouts.appBody.js';
 import '../../ui/layouts/appBody.js';
 
 FlowRouter.route('/', {
