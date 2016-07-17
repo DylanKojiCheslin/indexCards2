711dbfc25f5d003e845c8b2e4ffd766b909cd44b
dylan <dylan.kojicheslin@gmail.com>
2016-07-17 04:42:20 -0600

    template for listOfIndividualTags

--- imports/ui/layouts/appBody.js
+++ imports/ui/layouts/appBody.js
@@ -2,4 +2,5 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import "../components/individualTag.html"
+import "../components/listOfIndividualTags.html"
 import './appBody.html';
