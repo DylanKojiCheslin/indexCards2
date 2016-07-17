7e9b524e9b5a9233191dbcf8ef86d4518d19d15a
dylan <dylan.kojicheslin@gmail.com>
2016-07-17 03:58:14 -0600

    validate data context for individualTag

--- imports/ui/components/individualTag.js
+++ imports/ui/components/individualTag.js
@@ -1,7 +1,15 @@
+import { Tag } from '../../api/Tag.js'
 import "./individualTag.html"
 
 Template.individualTag.helpers({
   create: function(){
      //subscribe here
   },
+});
+
+Template.individualTag.onCreated(function() {
+  this.autorun(() => {
+    new SimpleSchema(Tag.getSchema())
+    .validate(Template.currentData());
+  });
 });
