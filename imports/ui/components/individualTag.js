6e704a3f25d351038a53ce13d035ceb6dfb63f84
dylan <dylan.kojicheslin@gmail.com>
2016-07-17 04:32:54 -0600

    cruft

--- imports/ui/components/individualTag.js
+++ imports/ui/components/individualTag.js
@@ -1,12 +1,6 @@
 import { Tag } from '../../api/Tag.js'
 import "./individualTag.html"
 
-Template.individualTag.helpers({
-  create: function(){
-     //subscribe here
-  },
-});
-
 Template.individualTag.onCreated(function() {
   this.autorun(() => {
     new SimpleSchema(Tag.getSchema())
